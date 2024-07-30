4d608e9cbf709b0ca38ffbb073e6255e1ae39f49
ruandocini <57142259+ruandocini@users.noreply.github.com>
2024-07-29 23:52:46 -0300

    Update test.py

--- test.py
+++ test.py
@@ -43,9 +43,13 @@ model = PeftModel.from_pretrained(model, peft_model_id)
 data = load_dataset("csv", data_files={"train":["bird_dev.csv"]})
 data = data.map(lambda samples: tokenizer(samples['train_example']), batched=True)
 
-raise ValueError(data)
+for batch in data:
+    print(batch)
+    with torch.cuda.amp.autocast():
+        output_tokens = model.generate(**batch, max_new_tokens=100)
+        print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
 
-with torch.cuda.amp.autocast():
-  output_tokens = model.generate(**batch, max_new_tokens=100)
+# with torch.cuda.amp.autocast():
+#   output_tokens = model.generate(**batch, max_new_tokens=100)
 
-print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
+# print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
