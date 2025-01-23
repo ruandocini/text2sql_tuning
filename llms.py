c2e833c961e784e8a8ab1ad388813ea0a6164c2f
ruandocini <ruandocini@gmail.com>
2025-01-23 00:39:35 -0300

    updated process to process whole database

--- llms.py
+++ llms.py
@@ -1,21 +1,37 @@
 from openai import OpenAI
+from langchain_ollama.llms import OllamaLLM
 
-class OpenAIClient:
-    def __init__(self, api_key):
-        self.api_key = api_key
+class LLMClient():
+    def __init__(self, model_name):
+        pass 
 
     def make_request(self, prompt):
-        client = OpenAI()
-        completion = client.chat.completions.create(
-            model="gpt-4",
+        pass
+
+class OpenAIClient(LLMClient):
+    def __init__(self, model_name="gpt-4"):
+        self.client = OpenAI()
+        self.model_name = model_name
+
+    def make_request(self, prompt):
+        completion = self.client.chat.completions.create(
+            model=self.model_name,
             messages=[
                 {"role": "user", "content": prompt}
             ],
             temperature=0.001
         )
         return completion.choices[0].message.content
+    
+class OllamaClient(LLMClient):
+    def __init__(self, model_name="llama3.2:1b"):
+        self.model_name = model_name
+        self.client = OllamaLLM(model=model_name, temperature=0.001)
 
-SIMPLE_PROMPT = """
+    def make_request(self, prompt):
+        return self.client.invoke(prompt, )
+
+SIMPLE_PROMPT_COLUMN = """
 You are supplied with the content of a specific column from a database and its current name.
 This name is not representative, meaning it does not accurately describe the content of the column.
 You are tasked with rephrasing the name of the column to better reflect its content.
@@ -24,7 +40,22 @@ The current column name is: "{column_name}"
 The content of the column is as follows: "{content}"
 
 Your reponse should come in the following format:
-{{"repahrsed_column_name": "new_column_name"}}
+{{"rephrased_column_name": "new_column_name"}}
+
+And only that, nothing more is accepted.
+Only generate a new name for per column.
+"""
+
+SIMPLE_PROMPT_TABLE = """
+You are supplied with the content of a specific table from a database and its current name.
+This name is not representative, meaning it does not accurately describe the content of the table.
+You are tasked with rephrasing the name of the table to better reflect its content.
+Remember that this name should be simple and also descriptive.
+The current table name is: "{table_name}"
+The content of the talbe is as follows: "{content}"
+
+Your reponse should come in the following format:
+{{"rephrased_table_name": "new_table_name"}}
 
 And only that, nothing more is accepted.
 """
--- rephrase.py
+++ rephrase.py
@@ -1,27 +1,68 @@
 import json
 import sqlite3
-from llms import OpenAIClient, SIMPLE_PROMPT
+from llms import SIMPLE_PROMPT_COLUMN, SIMPLE_PROMPT_TABLE, OllamaClient, LLMClient
+from typing import List
 
 class Rephrase:
-    def __init__(self, database_dir: str, schema: dict):
-        self.database_dir = database_dir
-        self.schema = schema
+    def __init__(self, model:LLMClient):
+        self.model = model
 
-    def rephrase_column_names(self, use_all_columns: bool = False, top_k_rows: int = 200) -> str:
+    def rephrase_full_database_columns(self, database:dict, database_dir:str, top_k_rows: int = 20) -> str:
+        for individual_databases in database.keys():
+            extract = mapper_of_columns[individual_databases]["tables"]
+            schema = {
+                key: list(mapper_of_columns[individual_databases]["columns"][key].values()) for key in extract.keys()
+            }
+            rephrased_column = self.rephrase_column_names(database_dir=database_dir, schema=schema, top_k_rows=top_k_rows)
+            for table in rephrased_column.keys():
+                for column_original, column_mod in list(mapper_of_columns[individual_databases]["columns"][table].items()):
+                    mapper_of_columns[individual_databases]["columns"][table][column_original] = rephrased_column[table][column_mod]
+
+        return mapper_of_columns
+
+
+    def rephrase_column_names(self, schema:dict, database_dir:str, use_all_columns: bool = False, top_k_rows: int = 20) -> str:
 
         mapping = {}
 
-        for database in self.schema.keys():
-            with sqlite3.connect(self.database_dir) as conn:
+        for table in schema.keys():
+            mapping.update({table: {}})
+            with sqlite3.connect(database_dir) as conn:
                 cursor = conn.cursor()
-                cursor.execute(f"SELECT * FROM {database}")
+                cursor.execute(f"SELECT * FROM {table}")
                 database_content = cursor.fetchall()
-                openai = OpenAIClient("API_KEY")
-                for idx, column in enumerate(self.schema[database]):
+                for idx, column in enumerate(schema[table]):
                     column_sample = [content[idx] for content in database_content][0:top_k_rows]
-                    rephrased_column = openai.make_request(SIMPLE_PROMPT.format(column_name=column, content=column_sample))
-                    mapping.update({column: rephrased_column})
-                    print(f"Ran algorithm for {idx} of {len(self.schema[database])} columns.")
+                    rephrased_column = self.model.make_request(SIMPLE_PROMPT_COLUMN.format(column_name=column, content=column_sample))
+                    try:
+                        # print(rephrased_column)
+                        rephrased_column = json.loads(rephrased_column)["rephrased_column_name"]
+                    except:
+                        rephrased_column = column
+
+                    mapping[table].update({column: rephrased_column})
+                    # print(f"Ran algorithm for {idx} of {len(schema[table])} columns.")
+            print(f"Ran algorithm for {table}.")
+
+        return mapping
+    
+    def rephrase_table_names(self, schema:dict, database_dir:str, use_specific_columns:List[str] = [], top_k_rows: int = 30) -> str:
+
+        mapping = {}
+
+        for table in schema.keys():
+            with sqlite3.connect(database_dir) as conn:
+                cursor = conn.cursor()
+                cursor.execute(f"SELECT * FROM {table}")
+                database_content = cursor.fetchall()
+                if len(use_specific_columns) == 0:
+                    table_sample = database_content[0:top_k_rows]
+                    rephrased_table = self.model.make_request(SIMPLE_PROMPT_TABLE.format(table_name=table, content=table_sample))
+                    try:
+                        rephrased_table = json.loads(rephrased_table)["rephrased_table_name"]
+                    except:
+                        rephrased_table = table
+                    mapping.update({table: rephrased_table})
 
         return mapping
 
@@ -32,24 +73,38 @@ if __name__ == "__main__":
         mapper_of_columns = file.read()
         mapper_of_columns = json.loads(mapper_of_columns)
 
-    extract = mapper_of_columns["california_schools"]["columns"]["frpm"]
+    model = OllamaClient("llama3.2:1b")
+    rephrase = Rephrase(model)
 
-    schema = {
-        "frpm": list(extract.values())[0:10]
-    }
+    print("Mapper before")
+    print(mapper_of_columns["california_schools"]["columns"]["schools"])
 
-    original_schema = {
-        "frpm": list(extract.keys())[0:10]
-    }
-    
-    rephrase = Rephrase(database_dir, schema)
-    rephrased_column = rephrase.rephrase_column_names()
+    california_schools = {"california_schools":mapper_of_columns["california_schools"]}
 
-    rephrased_schema = {
-        "frpm": list(rephrased_column.values())
-    }
+    mapper_of_columns["california_schools"]["columns"] = rephrase.rephrase_full_database_columns(
+        database=california_schools,
+        database_dir=database_dir,
+        top_k_rows=4
+    )["california_schools"]["columns"]
 
-    for idx, column in enumerate(original_schema["frpm"]):
-        print(f"Original column name: {column}")
-        print(f"Rephrased column name: {rephrased_schema['frpm'][idx]}")
-        print("")
+    print("Mapper after")
+    print(mapper_of_columns["california_schools"]["columns"]["schools"])
+
+
+    # original_schema = {
+    #     "frpm": list(extract.keys())[0:10]
+    # }
+
+    # rephrased_schema = {
+    #     "frpm": list(rephrased_column.values())
+    # }
+
+    # for idx, column in enumerate(original_schema["frpm"]):
+    #     print(f"Original column name: {column}")
+    #     print(f"Rephrased column name: {rephrased_schema['frpm'][idx]}")
+    #     print("")
+
+    ## EXPERIMENTOS A SE FAZER 
+    ## 1. até quando o nome da coluna vai mudando com a inserção de mais colunas para o modelo?
+    ## 2. Colocar barra de progresso para saber quanto tempo falta para terminar o processo para cada DB
+    ##
