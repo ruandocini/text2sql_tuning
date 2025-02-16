0f03f6ac3749f00bcab0fe53309a2c0ac9c77c5c
ruandocini <ruandocini@gmail.com>
2025-02-16 20:37:08 -0300

    fix decoded outputs

--- llms.py
+++ llms.py
@@ -80,4 +80,4 @@ class HuggingFaceClient(LLMClient):
         decoded_outputs = self.tokenizer.batch_decode(
             raw_outputs, skip_special_tokens=True
         )
-        return decoded_outputs
+        return decoded_outputs[0]
